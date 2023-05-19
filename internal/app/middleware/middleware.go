6616686eb340685c72daf7af5599c25a2bceaa87
evpost <kripsy93@yandex.ru>
2023-05-19 16:59:12 +0300

    add log response

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -11,14 +11,28 @@ import (
 // RequestLogger — middleware-логер для входящих HTTP-запросов.
 func RequestLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// start duration work of handler
 		start := time.Now()
-		// Код для измерения
-		next.ServeHTTP(w, r)
+
+		responseData := &responseData{
+			status: 0,
+			size:   0,
+		}
+
+		lw := loggingResponseWriter{
+			ResponseWriter: w,            // insert orig http.ResponseWriter
+			responseData:   responseData, // insert our respData
+		}
+
+		next.ServeHTTP(&lw, r)
 		duration := time.Since(start)
-		logger.Log.Debug("got incoming HTTP request",
+		logger.Log.Info("got incoming HTTP request",
 			zap.String("URI", r.URL.String()),
 			zap.String("method", r.Method),
 			zap.Int64("duration (Nanoseconds)", duration.Nanoseconds()),
+			zap.Int64("status code", int64(responseData.status)),
+			zap.Int64("response size", int64(responseData.size)),
 		)
 	})
 }
