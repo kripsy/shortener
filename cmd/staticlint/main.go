081bb98dccbecf33158b18a3cce785e7dcbdf360
Постернак Евгений Валерьевич <EVaPosternak@sberbank.ru>
2023-08-30 14:10:58 +0300

    add analyzers

---
+++ cmd/staticlint/main.go
@@ -0,0 +1,61 @@
+// Staticlint is used for static code analysis. It includes the most necessary checks.
+package main
+
+import (
+	"github.com/masibw/goone"
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/multichecker"
+	"golang.org/x/tools/go/analysis/passes/assign"
+	"golang.org/x/tools/go/analysis/passes/atomic"
+	"golang.org/x/tools/go/analysis/passes/bools"
+	"golang.org/x/tools/go/analysis/passes/composite"
+	"golang.org/x/tools/go/analysis/passes/copylock"
+	"golang.org/x/tools/go/analysis/passes/deepequalerrors"
+	"golang.org/x/tools/go/analysis/passes/defers"
+	"golang.org/x/tools/go/analysis/passes/directive"
+	"golang.org/x/tools/go/analysis/passes/errorsas"
+	"golang.org/x/tools/go/analysis/passes/fieldalignment"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
+	"golang.org/x/tools/go/analysis/passes/printf"
+	"golang.org/x/tools/go/analysis/passes/shadow"
+	"golang.org/x/tools/go/analysis/passes/structtag"
+	"golang.org/x/tools/go/analysis/passes/tests"
+	"golang.org/x/tools/go/analysis/passes/timeformat"
+	"golang.org/x/tools/go/analysis/passes/unmarshal"
+	"golang.org/x/tools/go/analysis/passes/unreachable"
+	"golang.org/x/tools/go/analysis/passes/unusedresult"
+	"honnef.co/go/tools/staticcheck"
+)
+
+func main() {
+	mychecks := []*analysis.Analyzer{
+		printf.Analyzer,          // detect inconsistency of printf format strings and arguments.
+		shadow.Analyzer,          // detect shadowed variables.
+		structtag.Analyzer,       // detect error in structure tags.
+		assign.Analyzer,          // detect useless assignments.
+		atomic.Analyzer,          // check correct use sync package.
+		bools.Analyzer,           // check error in boolean func.
+		composite.Analyzer,       // check unkeyed composite literal.
+		copylock.Analyzer,        // check error in copy some objects (Mutex, GroupWait and etc).
+		deepequalerrors.Analyzer, // check incorrect use of deepequals.
+		defers.Analyzer,          // check for defers.
+		directive.Analyzer,       // check correct use directives.
+		errorsas.Analyzer,        // check second args is error in errors.Is().
+		fieldalignment.Analyzer,  // detect struct with less using memory if their fields is sorted.
+		nilfunc.Analyzer,
+		tests.Analyzer,
+		timeformat.Analyzer,
+		unmarshal.Analyzer,
+		unreachable.Analyzer,
+		unusedresult.Analyzer,
+		goone.Analyzer, // check sql query in loop
+	}
+
+	for _, v := range staticcheck.Analyzers {
+		mychecks = append(mychecks, v.Analyzer)
+	}
+
+	multichecker.Main(
+		mychecks...,
+	)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/kripsy/shortener
 go 1.20
 
 require (
+	github.com/Masterminds/squirrel v1.5.4
 	github.com/go-chi/chi/v5 v5.0.8
+	github.com/gogo/protobuf v1.3.2
 	github.com/golang-jwt/jwt/v4 v4.5.0
 	github.com/golang-migrate/migrate/v4 v4.16.2
 	github.com/golang/mock v1.6.0
@@ -13,11 +15,13 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/stretchr/testify v1.8.2
 	go.uber.org/zap v1.24.0
+	golang.org/x/tools v0.12.0
 )
 
 require (
-	github.com/Masterminds/squirrel v1.5.4 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gostaticanalysis/analysisutil v0.6.1 // indirect
+	github.com/gostaticanalysis/comment v1.4.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -26,11 +30,17 @@ require (
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
 	github.com/lib/pq v1.10.9 // indirect
+	github.com/masibw/goone v1.4.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rogpeppe/go-internal v1.10.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp/typeparams v0.0.0-20221208152030-732eee02a75a // indirect
+	golang.org/x/mod v0.12.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	honnef.co/go/tools v0.4.5 // indirect
 )
